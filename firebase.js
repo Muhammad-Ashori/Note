ebaa4662efd662c6ed4ea76d02c2708539297ca0
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2023-12-01 20:32:11 +0430

    creat firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJOZwRup_Shd8puNK3CmKDLRX9RmZOl30",
+  authDomain: "react-notes-9f780.firebaseapp.com",
+  projectId: "react-notes-9f780",
+  storageBucket: "react-notes-9f780.appspot.com",
+  messagingSenderId: "945340165721",
+  appId: "1:945340165721:web:9d13c05892158bd84e4b48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
